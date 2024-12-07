6027fb097a6016c4ebcd2f6b470f991cbb9f38ea
LindembergueFrank <lindemberg.frank@gmail.com>
2024-12-07 17:20:40 -0300

    feat: improving the controller

--- src/main/java/com/repospring/management_guests/controller/GuestsController.java
+++ src/main/java/com/repospring/management_guests/controller/GuestsController.java
@@ -1,13 +1,23 @@
 package com.repospring.management_guests.controller;
 
+import com.repospring.management_guests.repository.Guests;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GuestsController {
 
+    @Autowired
+    private Guests guests;
+
     @GetMapping("/guests")
-    public String listing() {
-        return "ListGuests";
+    public ModelAndView listing() {
+        ModelAndView modelAndView = new ModelAndView("ListGuests");
+
+        modelAndView.addObject("guests", guests.findAll());
+
+        return modelAndView;
     }
 }
