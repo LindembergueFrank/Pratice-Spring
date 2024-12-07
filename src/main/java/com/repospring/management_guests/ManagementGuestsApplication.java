7bc91d95a6edc0a3a0d247a2ea439ae9ec0101d2
LindembergueFrank <lindemberg.frank@gmail.com>
2024-12-07 16:22:39 -0300

    feat: config initial

---
+++ src/main/java/com/repospring/management_guests/ManagementGuestsApplication.java
@@ -0,0 +1,13 @@
+package com.repospring.management_guests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementGuestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementGuestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repospring/management_guests/ManagementGuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repospring.management_guests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementGuestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
