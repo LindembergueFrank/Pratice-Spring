64c09b1632f58257e7c60280faa226bc19fb5e83
LindembergueFrank <lindemberg.frank@gmail.com>
2024-12-16 18:19:35 -0300

    init: starting project

---
+++ spring-example2/src/main/java/com/repojava/spring_example2/SpringExample2Application.java
@@ -0,0 +1,13 @@
+package com.repojava.spring_example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExample2Application.class, args);
+	}
+
+}
